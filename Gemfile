5e94d4afe983774cfbed652511a9c858734a4c1f
omohayui <omohayui@example.com>
2015-02-13 19:32:14 +0900

    Add scss layout

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.5'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
 group :development, :test do
     gem 'sqlite3', '1.3.8'
@@ -35,3 +37,4 @@ group :production do
     gem 'pg', '0.15.1'
     gem 'rails_12factor', '0.0.2'
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,7 +54,11 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/rails'
-include Capybara::DSL
+#include Capybara::DSL
+
+RSpec.configure do |config|
+      config.include Capybara::DSL, :type => :feature
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
