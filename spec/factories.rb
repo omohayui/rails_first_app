48170d1779844c0f9139506ba323e894efcdde0d
omohayui <omohayui@example.com>
2015-02-16 19:04:58 +0900

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name     "Omoha Yui"
+        email    "omohayui@example.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
